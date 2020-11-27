85860dfb99ea9c58f16900cbcd08694a24c80977
BuildTools <unconfigured@null.spigotmc.org>
2020-11-27 18:52:30 +0100

    Updated discord strategy

--- src/strategies/discordstrat.js
+++ src/strategies/discordstrat.js
@@ -1,11 +1,37 @@
 const DiscordStrategy = require('passport-discord').Strategy;
-const passport = require('passport')
+const passport = require('passport');
+const DiscordUser = require('../models/DiscordUser')
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    const user = await DiscordUser.findById(id);
+    if(user) 
+        done(null, user);
+});
 
 passport.use(new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.CLIENT_REDIRECT,
-    scope: ['identify', 'guilds', 'email']
-}, (accessToken, refreshToken, profile, done) => {
-    console.log(profile.email);
+    scope: ['identify', 'guilds']
+}, async (accessToken, refreshToken, profile, done) => {
+    try {
+        const user = await DiscordUser.findOne({ discordId: profile.id });
+        if(user)
+            done(null, user);
+        else {
+            const newUser = await DiscordUser({
+                discordId: profile.id,
+                username: profile.username
+            });
+            const savedUser = await newUser.save();
+            done(null, savedUser);
+        }
+    } catch(err) {
+        console.log(err);
+        done(err, null);
+    }
 }));
